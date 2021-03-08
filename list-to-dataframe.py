5284e8e09dfbeccbf04a7d0315de051efbfe268e
Indraneel Chakraborty <64887729+ineelhere@users.noreply.github.com>
2021-03-08 21:44:10 +0530

    Convert list to a dataframe

---
+++ list-to-dataframe.py
@@ -0,0 +1,12 @@
+mountains = ("K2", "Kangchenjunga", "Nanda Devi", "Kamet", "Saltoro Kangri", "Saser Kangri")
+import pandas as pd
+dataframe = pd.DataFrame({"mountains in India":mountains})
+dataframe
+
+#    mountains in India
+# 0  K2
+# 1  Kangchenjunga
+# 2  Nanda Devi
+# 3  Kamet
+# 4  Saltoro Kangri
+# 5  Saser Kangri
