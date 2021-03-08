e96af5c2490ddcab4fcf3e2a0401089235a1d9a6
Indraneel Chakraborty <64887729+ineelhere@users.noreply.github.com>
2021-03-08 22:06:03 +0530

    Create a dictionary and print it's keys & values

---
+++ dictionary-values-keys.py
@@ -0,0 +1,8 @@
+dictionary = {"mountains":("K2", "Kangchenjunga", "Nanda Devi", "Kamet", "Saltoro Kangri", "Saser Kangri"),
+             "height":(8611, 8586, 7816, 7756, 7742, 7672)}
+print(dictionary.values())
+print(dictionary.keys())
+
+#outputs
+# dict_values([('K2', 'Kangchenjunga', 'Nanda Devi', 'Kamet', 'Saltoro Kangri', 'Saser Kangri'), (8611, 8586, 7816, 7756, 7742, 7672)])
+# dict_keys(['mountains', 'height'])
