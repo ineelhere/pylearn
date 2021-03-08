ae1024b1bced79646461b3a63161a8001568ea44
Indraneel Chakraborty <64887729+ineelhere@users.noreply.github.com>
2021-03-08 21:32:07 +0530

    Replace any character/word in a string

---
+++ replace-str-characters.py
@@ -0,0 +1,15 @@
+s = "To be, or not to be, that is the question"
+s = s.replace("be", "")
+print(s) #To , or not to , that is the question
+
+s = "To be, or not to be, that is the question"
+s = s.replace("to", "")
+print(s) #To be, or not  be, that is the question
+
+s = "To be, or not to be, that is the question"
+s = s.replace("ques", "")
+print(s) #To be, or not to be, that is the tion
+
+s = "To be, or not to be, that is the question"
+s = s.replace(" ", "")
+print(s) #Tobe,ornottobe,thatisthequestion
