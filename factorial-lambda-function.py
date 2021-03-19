a5d7a080bb9a95f4c78f30358644d5552eb7eb64
Indraneel Chakraborty <64887729+ineelhere@users.noreply.github.com>
2021-03-19 21:31:51 +0530

    Factorial using lambda function

---
+++ factorial-lambda-function.py
@@ -0,0 +1,4 @@
+# finding factorial of a number using recursive function
+facto = lambda n: 1 if n==1 else (n*facto(n-1))
+n = int(input("Please enter the number for calculating factorial - " ))
+facto(n)
