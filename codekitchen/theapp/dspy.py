f702e3461fcb780babc57c306e0465689a92663a
Indraneel Chakraborty <64887729+ineelhere@users.noreply.github.com>
2021-08-30 08:04:05 +0530

    Update and rename app.py to dspy.py

---
+++ codekitchen/theapp/dspy.py
@@ -0,0 +1,47 @@
+import streamlit as st
+from py101 import *
+
+st.sidebar.subheader("About dspy")
+st.sidebar.info("**A webapp that is running on python and teaching python!**")
+st.sidebar.markdown("""
+<img src="https://media.giphy.com/media/3o7527pa7qs9kCG78A/giphy.gif" width="200"><br>
+<hr>
+
+## Meet the Team  
+
+**Development and Strategy**
+- [Bhanu Chandrika Chitta](https://www.linkedin.com/in/chitta-bhanu-chandrika/)
+- [Indraneel Chakraborty](https://www.linkedin.com/in/indraneelchakraborty/)
+
+**Quality Review and Resource Management **
+- [Puja Roychowdhury](https://www.linkedin.com/in/pujarc98/)
+- [Sneha Podder](http://www.linkedin.com/in/snehapodder)
+
+___
+**© dspy | 2021 | with ❤️ from India**
+""", unsafe_allow_html=True)
+
+st.title("`dspy` - Data Science with Python")
+st.markdown("""
+___
+""")
+
+st.subheader("Please select what you would like to do")
+features = ["python 101 - Learn the basics of python",
+            "pyPrac - Solve problems using python",
+            "pandas - Learn data analysis and manipulation",]
+selection = st.radio("", features)
+if selection == features[0]:
+    st.balloons()
+    first()
+    helloworld()
+    # numbers()
+else:
+    st.write("![](https://media3.giphy.com/media/STZxU3AXEdwW4caLwS/giphy.gif?cid=790b761115e96593923fc6494cb027cacde63a309c048f29&rid=giphy.gif&ct=g)")
+
+
+st.markdown("""
+___
+**© dspy | 2021 | with ❤️ from India**
+""")
+
