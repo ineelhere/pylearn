5dd774c9c75cff9158282222342aa59e9bd0bc4c
Chitta V P Bhanu Chandrika <50956420+bhanuchandrika@users.noreply.github.com>
2021-08-31 19:06:04 +0530

    Welcome to the world of python!!

---
+++ codekitchen/py_files/hello-world-dspy.py
@@ -0,0 +1,6 @@
+print('Hello World!!')
+print('Get ready to rock-N-roll ')
+# we saw that the lines automatically shifted to new lines.
+# although we can do this manually too, in one print statement
+print('Hello World!! \nGet ready to rock-N-roll')
+# and that's how we do it !!
