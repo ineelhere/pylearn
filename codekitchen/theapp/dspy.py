89a27a2eefa92a2d05bdb5c139cec49c2b8bf9b7
Indraneel Chakraborty <64887729+ineelhere@users.noreply.github.com>
2021-09-05 01:52:08 +0530

    content update

--- codekitchen/theapp/dspy.py
+++ codekitchen/theapp/dspy.py
@@ -7,6 +7,10 @@ st.sidebar.subheader("About dspy")
 st.sidebar.info("**A webapp that is running on python and teaching python!**")
 st.sidebar.markdown("""
 <img src="https://media.giphy.com/media/3o7527pa7qs9kCG78A/giphy.gif" width="200"><br>
+  \n\n`dspy` is an open-source initiative to help individuals learn/teach the basics of python with streamlit - which also runs on python. `dspy` focuses more on the application of python in Data Science.
+  \n\nIsn't it nice that one is actually using python to learn/teach python?! This gives an idea of what can be done with python (beyond what has been discussed in this webapp).
+  \n\nWe encourage collaborations and usage of this webapp for education/training purposes.
+  \n\nFor any queries/feedback please feel free to connect with the team on LinkedIn.
 ___
 ## Meet the Team  
 **Development and Strategy**
@@ -57,3 +61,4 @@ ___
 **© dspy | 2021 | with ❤️ from India**
 """)
 st.warning("**Development in progress. More features are on the way!**")
+
--- codekitchen/theapp/pyApps.py
+++ codekitchen/theapp/pyApps.py
@@ -3,10 +3,15 @@ from translate_text import *
 
 def pyApps():
     st.header("**`pyApps` - Some simple data apps made with python**")
-    selections = ["Translate text using Python"]
+    selections = ["Translate text using Python", "Realtime stats on the COVID19 situation in India"]
     st.subheader("**Here are the apps which are available (more on the way!)**")
     response = st.radio('', selections)
     st.markdown("___")
 
+    if response == selections[1]:
+        st.success("""
+        This webapp is available here - **[https://share.streamlit.io/ineelhere/ifc19/2.0/ifc19_app.py](https://share.streamlit.io/ineelhere/ifc19/2.0/ifc19_app.py)**
+          \nPlease click on the above URL to visit the webapp.
+        """)
     if response == selections[0]:
         translate_text()
